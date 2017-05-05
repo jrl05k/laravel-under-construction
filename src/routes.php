7c9c3b04d16cacd8bf2c428f92eed4c9ac841300
Rob <jrl05k@gmail.com>
2017-05-04 22:47:21 -0400

    route namespacing

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 Route::group(['middleware' => ['web']], function () {
-    Route::get('/under-construction', 'Jrl05k\UnderConstruction\UnderConstructionController@index');
-    Route::get('/under-construction/login', 'Jrl05k\UnderConstruction\UnderConstructionController@login');
-    Route::post('/under-construction/login', 'Jrl05k\UnderConstruction\UnderConstructionController@authenticate');
-    Route::get('/under-construction/logout', 'Jrl05k\UnderConstruction\UnderConstructionController@logout');
+    Route::get('/under-construction', 'UnderConstruction\UnderConstructionController@index');
+    Route::get('/under-construction/login', 'UnderConstruction\UnderConstructionController@login');
+    Route::post('/under-construction/login', 'UnderConstruction\UnderConstructionController@authenticate');
+    Route::get('/under-construction/logout', 'UnderConstruction\UnderConstructionController@logout');
 });
